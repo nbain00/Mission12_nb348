148c7a0c61dfedd281eafcd3db287ebd49af623b
nbain00 <zink47@gmail.com>
2023-03-22 13:43:34 -0600

    Initial

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+import TeamList from './Teams'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <TeamList/>
+    </div>
+  );
+}
+
+export default App;
